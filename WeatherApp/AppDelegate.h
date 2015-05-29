0043b90741a057e3136e5e37dc1464f6ad1ec6b7
Benjamin Steephenson <benjaminsteephenson@gmail.com>
2015-05-29 09:46:29 -0400

    Initial Commit

---
+++ WeatherApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherApp
+//
+//  Created by Benjamin Steephenson on 5/29/15.
+//  Copyright (c) 2015 Benjamin Steephenson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeatherApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeatherApp
+//
+//  Created by Benjamin Steephenson on 5/29/15.
+//  Copyright (c) 2015 Benjamin Steephenson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
